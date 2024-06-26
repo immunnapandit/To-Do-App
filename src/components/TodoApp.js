fa26341a703b8fdfc1397c830f7fdd36a92a3f31
MUNNA KUMAR <munnapandit2002@gmail.com>
2024-06-26 22:36:05 +0530

    Components Created

---
+++ src/components/TodoApp.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './TodoApp.css';
+
+const TodoApp = () => {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+
+  const handleAddTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="todo-app">
+      <h1>To-Do List</h1>
+      <div>
+        <input 
+          type="text" 
+          value={task} 
+          onChange={(e) => setTask(e.target.value)} 
+          placeholder="Add a new task" 
+        />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task} 
+            <button onClick={() => handleDeleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoApp;
